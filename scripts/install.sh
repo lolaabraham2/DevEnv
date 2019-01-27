e8134487d2404faf4d63f3c588afde7936f7d0c3
ROME84 <22250311+Rome84@users.noreply.github.com>
2019-01-27 01:45:46 -0500

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -43,5 +43,12 @@ P_RETVAL=$?
 && unzip -o packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin \
 && rm packer_${PACKER_VERSION}_linux_amd64.zip
 
+#kuctl
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
 # clean up
 apt-get clean
